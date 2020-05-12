db9737923d6f5afd19866f07d86c0fbdcdb394db
bowen zhang <bowen0225@users.noreply.github.com>
2020-05-12 23:08:54 +1000

    revert database connecton

--- content/connect_database.php
+++ content/connect_database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbName = 'SePay';
+$host = 'sepay.coqnkhi2ftwp.us-east-1.rds.amazonaws.com';
+$dbName = 'sepay';
 $db_user = 'root';
-$db_pass = 'root';
+$db_pass = 'rootroot';
 $dsn = "mysql:host=$host;port=3306;dbname=$dbName";
 $db = new PDO($dsn, $db_user, $db_pass);
 
