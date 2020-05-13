55337fc3d5249731bd238d40d3dbe2a4d85acb3e
rmit-s3702629-Zhaoxu-Nie <48911665+rmit-s3702629-Zhaoxu-Nie@users.noreply.github.com>
2020-05-13 21:26:46 +1000

    manage branch

--- content/connect_database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'sepay.coqnkhi2ftwp.us-east-1.rds.amazonaws.com';
-$dbName = 'sepay';
-$db_user = 'root';
-$db_pass = 'rootroot';
-$dsn = "mysql:host=$host;port=3306;dbname=$dbName";
-$db = new PDO($dsn, $db_user, $db_pass);
-?>
